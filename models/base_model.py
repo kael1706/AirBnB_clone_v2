d464b4033a4c425e7c3cb08293d364006e8a04db
kael1706 <cdcp-1795@hotmail.com>
2020-04-22 19:56:46 +0000

    try to fix a7

--- models/base_model.py
+++ models/base_model.py
@@ -50,11 +50,16 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                if key != "__class__" and hasattr(self, key):
                     setattr(self, key, value)
+                if self.created_at is None:
+                    self.created_at = datetime.now()
+                if self.updated_at is None:
+                    self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """returns a string
